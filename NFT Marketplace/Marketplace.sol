f60357e1bb0fbb4896f9d2174adf894665997f11
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2022-09-27 18:16:32 +0530

    Update Marketplace.sol

--- NFT Marketplace/Marketplace.sol
+++ NFT Marketplace/Marketplace.sol
@@ -10,6 +10,7 @@ contract Marketplace is ReentrancyGuard {
     address payable public immutable feeAccount; 
     uint public immutable feePercent; 
     uint public itemCount; 
+    enum state{created,release}
 
     struct Item {
         uint itemId;
@@ -19,70 +20,50 @@ contract Marketplace is ReentrancyGuard {
         address payable seller;
         bool sold;
     }
-
+    // enum -> State of the nft
+    state State;
+    // mapping of Items
     mapping(uint => Item) public items;
 
-    event Offered(
-        uint itemId,
-        address indexed nft,
-        uint tokenId,
-        uint price,
-        address indexed seller
-    );
-    event Bought(
-        uint itemId,
-        address indexed nft,
-        uint tokenId,
-        uint price,
-        address indexed seller,
-        address indexed buyer
-    );
+// Events
+    event Offered(uint itemId,address indexed nft,uint tokenId,uint price,address indexed seller);
+    event Bought(uint itemId,address indexed nft,uint tokenId,uint price,address indexed seller,address indexed buyer);
 
     constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
 
-    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+    function list(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
         require(_price > 0, "Price must be greater than zero");
         itemCount ++;
         _nft.transferFrom(msg.sender, address(this), _tokenId);
-        items[itemCount] = Item (
-            itemCount,
-            _nft,
-            _tokenId,
-            _price,
-            payable(msg.sender),
-            false
-        );
-        emit Offered(
-            itemCount,
-            address(_nft),
-            _tokenId,
-            _price,
-            msg.sender
-        );
+        items[itemCount] = Item (itemCount,_nft, _tokenId,_price,payable(msg.sender),false);
+
+        emit Offered(itemCount,address(_nft),_tokenId,_price,msg.sender);
+
+        State = state.created; 
     }
 
     function purchaseItem(uint _itemId) external payable nonReentrant {
+
         uint _totalPrice = getTotalPrice(_itemId);
         Item storage item = items[_itemId];
+
         require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
         require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
         require(!item.sold, "item already sold");
+
         item.seller.transfer(item.price);
         feeAccount.transfer(_totalPrice - item.price);
         item.sold = true;
+
         item.nft.transferFrom(address(this), msg.sender, item.tokenId);
-        emit Bought(
-            _itemId,
-            address(item.nft),
-            item.tokenId,
-            item.price,
-            item.seller,
-            msg.sender
-        );
+
+        emit Bought(_itemId,address(item.nft),item.tokenId,item.price,item.seller,msg.sender);
+        State = state.release;
     }
+
     function getTotalPrice(uint _itemId) view public returns(uint){
         return((items[_itemId].price*(100 + feePercent))/100);
     }
